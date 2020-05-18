c090056498bb295862064e2920110e0cbd58febd
Jacob Andrean <andreanjacob99@gmail.com>
2020-05-18 18:03:31 +0700

    Storyboard v2

---
+++ TestVC.swift
@@ -0,0 +1,30 @@
+//
+//  TestVC.swift
+//  dorra
+//
+//  Created by Jacob Andrean on 18/05/20.
+//  Copyright © 2020 MC2G7. All rights reserved.
+//
+
+import UIKit
+
+class TestVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

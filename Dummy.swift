c5abbf071496c2a9fd8496e6fd39fbd37524f6cc
yohan <yohan@binus.ac.id>
2020-05-20 13:32:19 +0700

    dummy commented

--- Dummy.swift
+++ Dummy.swift
@@ -9,32 +9,32 @@
 import Foundation
 import CoreData
 
-extension Students {
-    
-    static func fetchAll(viewContext: NSManagedObjectContext) -> [Students] {
-        let request: NSFetchRequest<Students> = Students.fetchRequest()
-        let result = try? viewContext.fetch(request)
-        return result ?? [] 
-    }
-    
-    static func save(viewContext: NSManagedObjectContext ) -> Students? {
-        let student = Students(context: viewContext)
-        student.studentName = "Axella"
-        student.studentUniversity = "university"
-        student.studentEmail = "Axella123@gmail.com"
-        do {
-            try viewContext.save()
-            return student
-        } catch {
-            return nil
-        }
-    }
-    
-    static func deleteAll(viewContext: NSManagedObjectContext) {
-        
-        let request: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Students")
-        let deleteRequest = NSBatchDeleteRequest(fetchRequest: request)
-        let _ = try? viewContext.execute(deleteRequest)
-    }
-    
-}
+//extension Students {
+//
+//    static func fetchAll(viewContext: NSManagedObjectContext) -> [Students] {
+//        let request: NSFetchRequest<Students> = Students.fetchRequest()
+//        let result = try? viewContext.fetch(request)
+//        return result ?? []
+//    }
+//
+//    static func save(viewContext: NSManagedObjectContext ) -> Students? {
+//        let student = Students(context: viewContext)
+//        student.studentName = "Axella"
+//        student.studentUniversity = "university"
+//        student.studentEmail = "Axella123@gmail.com"
+//        do {
+//            try viewContext.save()
+//            return student
+//        } catch {
+//            return nil
+//        }
+//    }
+//
+//    static func deleteAll(viewContext: NSManagedObjectContext) {
+//
+//        let request: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Students")
+//        let deleteRequest = NSBatchDeleteRequest(fetchRequest: request)
+//        let _ = try? viewContext.execute(deleteRequest)
+//    }
+//
+//}

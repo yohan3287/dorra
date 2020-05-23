1c57155c0cd734d20d586b5ea463343a22e0192d
yohan <yohan@binus.ac.id>
2020-05-24 00:30:45 +0700

    revisi select term di storyboard, penambahan pickerview

--- dorra/LecturerHistoryVC.swift
+++ dorra/LecturerHistoryVC.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class LecturerHistoryVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class LecturerHistoryVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UIPickerViewDelegate, UIPickerViewDataSource {
     
     @IBOutlet weak var historyCV: UICollectionView!
+    @IBOutlet weak var termTextField: UITextField!
     
-    var semesterList: [String] = ["sem 1", "sem 2", "sem 3"]
+    var termList: [String] = ["sem 1", "sem 2", "sem 3"]
     var courseName: [String] = ["Data Structure", "Database System", "Object Oriented", "Computer Network"]
     var totalClass: [Int] = [22, 16, 8, 10]
     var cellColor: [UIColor] = [#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1), #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1), #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)]
@@ -30,6 +31,40 @@ class LecturerHistoryVC: UIViewController, UICollectionViewDelegate, UICollectio
         
         return cell
     }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return termList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return termList[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        termTextField.text = termList[row]
+    }
+    
+    func createPickerView() {
+           let pickerView = UIPickerView()
+           pickerView.delegate = self
+           termTextField.inputView = pickerView
+    }
+    
+    func dismissPickerView() {
+       let toolBar = UIToolbar()
+       toolBar.sizeToFit()
+        let button = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.action))
+       toolBar.setItems([button], animated: true)
+       toolBar.isUserInteractionEnabled = true
+       termTextField.inputAccessoryView = toolBar
+    }
+    
+    @objc func action() {
+          view.endEditing(true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,14 +72,18 @@ class LecturerHistoryVC: UIViewController, UICollectionViewDelegate, UICollectio
         // Do any additional setup after loading the view.
         self.historyCV.dataSource = self
         self.historyCV.delegate = self
+        
+        createPickerView()
+        dismissPickerView()
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
     }
     
-    @IBAction func selectSemester(_ sender: UIButton) {
+    @IBAction func termTextFieldEditingDidEnd(_ sender: UITextField) {
         self.historyCV.reloadData()
+        
     }
     
     /*

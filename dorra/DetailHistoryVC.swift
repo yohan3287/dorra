ffcf9386f3565a42707039373d60882bc9f4ffaa
yohan <yohan@binus.ac.id>
2020-05-25 16:01:59 +0700

    layouting element storyboard

---
+++ dorra/DetailHistoryVC.swift
@@ -0,0 +1,30 @@
+//
+//  DetailHistoryVC.swift
+//  dorra
+//
+//  Created by Yohan Wongso on 25/05/20.
+//  Copyright © 2020 MC2G7. All rights reserved.
+//
+
+import UIKit
+
+class DetailHistoryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

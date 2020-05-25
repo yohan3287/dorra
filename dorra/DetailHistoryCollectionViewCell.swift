c26fe1de4fdd48e5ada076a9f16726030a42b946
yohan <yohan@binus.ac.id>
2020-05-25 16:40:41 +0700

    buat DetailHistoryCVC, code CV

---
+++ dorra/DetailHistoryCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  DetailHistoryCollectionViewCell.swift
+//  dorra
+//
+//  Created by Yohan Wongso on 25/05/20.
+//  Copyright © 2020 MC2G7. All rights reserved.
+//
+
+import UIKit
+
+class DetailHistoryCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var linkLabel: UILabel!
+    @IBOutlet weak var noteLabel: UILabel!
+}
--- dorra/DetailHistoryVC.swift
+++ dorra/DetailHistoryVC.swift
@@ -8,8 +8,31 @@
 
 import UIKit
 
-class DetailHistoryVC: UIViewController {
-
+class DetailHistoryVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return t.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = detailHistoryCV.dequeueReusableCell(withReuseIdentifier: "detailCell", for: indexPath) as! DetailHistoryCollectionViewCell
+        
+        cell.numberLabel.text = String(indexPath.row + 1)
+        cell.numberLabel.backgroundColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
+        cell.titleLabel.text = t[indexPath.row]
+        cell.linkLabel.text = l[indexPath.row]
+        cell.noteLabel.text = n[indexPath.row]
+        
+        return cell
+    }
+    
+    @IBOutlet weak var courseLabel: UILabel!
+    @IBOutlet weak var totalClassLabel: UILabel!
+    @IBOutlet weak var detailHistoryCV: UICollectionView!
+    
+    var t: [String] = ["title 1", "title 2", "title 3"]
+    var l: [String] = ["www.wibu1.com", "www.wibu2.com", "www.wibu3.jp"]
+    var n: [String] = ["anime for life", "gundam ampas", "anime bangku"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
